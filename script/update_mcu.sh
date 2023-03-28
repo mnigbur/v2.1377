e475cde4546047e6f35edcd0ba0e83345d81b9b5
Markus Nigbur <markus@mnigbur.de>
2023-03-28 07:39:48 +0200

    Added ERCF config

--- script/update_mcu.sh
+++ script/update_mcu.sh
@@ -7,7 +7,7 @@ function flash_mcu() {
   echo ""
   make clean
   #make menuconfig KCONFIG_CONFIG=$CONFIG_PATH/klipper_config.$1
-  make KCONFIG=$CONFIG_PATH/$1
+  make KCONFIG_CONFIG=$CONFIG_PATH/klipper_config.$1
   read -p "mcu $1 firmware built. Press [Enter] to flash"
   case $1 in
     rpi)
@@ -17,7 +17,8 @@ function flash_mcu() {
       ./scripts/flash-sdcard.sh /dev/ttyAMA0 fysetc-spider-v1
       ;;
     ercf)
-      #TODO: sudo /usr/local/bin/bossac -i -d -p /dev/ttyACM1 -e -w -v -R --offset=0x2000 out/klipper.bin
+      echo "Aborting! Please verify serial device!"
+      #sudo /usr/local/bin/bossac -i -d -p /dev/ttyACM1 -e -w -v -R --offset=0x2000 out/klipper.bin
     ;;
   esac
   echo "Finish update mcu $1"
@@ -29,6 +30,6 @@ sudo service klipper stop
 
 flash_mcu rpi
 flash_mcu spider
-#TODO: flash_mcu ercf
+flash_mcu ercf
 
 sudo service klipper start
