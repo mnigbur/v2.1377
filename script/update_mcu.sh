8084f7fa3b797231ef2e2364c12c9387d1d8c12d
Markus Nigbur <markus@mnigbur.de>
2025-01-02 11:32:02 +0100

    Updated klipper firmware update script with canbus version

--- script/update_mcu.sh
+++ script/update_mcu.sh
@@ -16,9 +16,14 @@ function flash_mcu() {
     spider)
       ./scripts/flash-sdcard.sh /dev/ttyAMA0 fysetc-spider-v1
       ;;
-    ercf)
-      sudo /usr/local/bin/bossac -i -d -p /dev/ttyACM0 -e -w -v -R --offset=0x2000 out/klipper.bin
-    ;;
+    toolhead)
+      python3 ~/katapult/scripts/flashtool.py -i can0 -u f7ad7cd429e2 -r
+      python3 ~/katapult/scripts/flashtool.py -i can0 -u f7ad7cd429e2 -f ~/klipper/out/klipper.bin
+      ;;
+    mmu)
+      python3 ~/katapult/scripts/flashtool.py -i can0 -u 13d7b8824ae8 -r
+      python3 ~/katapult/scripts/flashtool.py -i can0 -u 13d7b8824ae8 -f ~/klipper/out/klipper.bin
+      ;;
   esac
   echo "Finish update mcu $1"
   echo ""
@@ -29,6 +34,7 @@ sudo service klipper stop
 
 flash_mcu rpi
 flash_mcu spider
-flash_mcu ercf
+flash_mcu toolhead
+flash_mcu mmu
 
 sudo service klipper start
